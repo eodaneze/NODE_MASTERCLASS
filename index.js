d856305f15a444a4f2830a392178db0b4e29d0ee
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2023-01-06 14:19:49 -0800

    working with npm

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const date = require('./dateModel')
 const num = require('./addNumModel')
-const fs = require('fs')
+const uc = require("upper-case")
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(`the current date is ${date.currentDate()}`)
-    res.write(`the addition is ${num.addNum(10 , 20)}`)
+    // res.write(`the addition is ${num.addNum(10 , 20)}`)
+    res.write(uc.upperCase("this is node package manager"))
     res.write(req.url)
     res.end()
 }).listen(8000, () => {
